4d61e8006b151a30420acaa5395b8bfd5efb1fb7
Stephen Fairbanks <biggste2000@hotmail.com>
2016-07-21 11:28:41 +0100

    .gitignore update to include images

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/dist/images/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/dist/images/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/dist/images/favicon-16x16.png" sizes="16x16">
   <script src="https://use.typekit.net/opk1sck.js"></script>
   <script>try{Typekit.load({ async: true });}catch(e){}</script>
   <link href='https://fonts.googleapis.com/css?family=Yellowtail' rel='stylesheet' type='text/css'>
