5fbf443eb4a0d0256be6f58cae37e37ad06f5cfd
Stephen Fairbanks <biggste2000@hotmail.com>
2016-07-29 17:50:02 +0100

    Side Column updates

--- templates/includes/side-col-1.inc.php
+++ templates/includes/side-col-1.inc.php
@@ -9,6 +9,28 @@
 		</ul>
 	<?php } ?>
 
+<?php } elseif(is_page()) { ?>
+	
+	<h3>Did you know?</h3>
+
+	<?php 
+	// Array
+	$items = array(
+		'Chuck Norris can slam a revolving door.',
+		'Death once had a near-Chuck Norris experience.',
+		'Chuck Norris once kicked a horse in the chin. Its decendants are known today as Giraffes.',
+		'Chuck Norris can win a game of Connect Four in only three moves.',
+		'Chuck Norris can cut through a hot knife with butter.',
+		);
+	?>
+
+	<blockquote>
+		<?php
+		// ECHO OUT RANDOM THING
+		echo $items[array_rand($items)]; ?>
+	</blockquote>
+	<small>Facts from <a href="http://www.chucknorrisfacts.com/
+" target="_blank">CNF</a></small>
 <?php } else { ?>
 	
 	<h3>Social stuff</h3>
