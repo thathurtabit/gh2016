5509316056e7dcaad874a6ad85eba587428202b7
Stephen Fairbanks <biggste2000@hotmail.com>
2016-08-20 11:56:41 +0100

    Fixes for Google Eyes and Single Post (TR)

--- templates/includes/single-results-tr.inc.php
+++ templates/includes/single-results-tr.inc.php
@@ -1,1 +1,1 @@
-<h3>Something</h3>
+<span class="zigzag--blue"><svg id="zigzag" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 39.39 8.56"><defs></defs><title>zigzag</title><polyline id="zigzag-2" data-name="zigzag" points="0.62 1.27 8.25 7.28 15.88 1.27 23.5 7.28 31.14 1.27 38.77 7.28"></polyline></svg></span>
