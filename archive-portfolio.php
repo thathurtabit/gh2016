6162914fd32a05d3aa62b6cda715d55106222e31
Stephen Fairbanks <biggste2000@hotmail.com>
2016-08-23 19:09:17 +0100

    Attempting Safari Fixes

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,4 +1,4 @@
-<article <?php post_class('page-article section--border-white-sides-top'); ?>>
+<article <?php post_class('page-article entry-content section--border-white-sides-top'); ?>>
     
     <!-- DEFAULT HEADER -->
     <?php include( TEMPLATEPATH . '/templates/includes/header-default.inc.php' ); ?>
@@ -6,7 +6,7 @@
  
 
     <!-- ARTICLE ROW -->
-    <div class="article-row--blocks row section--background-white">
+    <div class="entry-content-inner content-list-block-wrap row section--background-white">
 
       
        <?php if (!have_posts()) : ?>
--- templates/content-work.php
+++ templates/content-work.php
@@ -1,11 +1,14 @@
-<div class="content-list-block">
-	<article <?php post_class('content-list-block-wrap'); ?>>
-	<div class="content-list-block__img">		
+<div class="article-divider waypoints">
+	<article <?php post_class(' row'); ?>>
+
+	<div class="article__thumb col-sm-3 col-md-12 col-md-offset-1">		
 		<!-- FEATURED THUMBNAIL -->
           <?php include( TEMPLATEPATH . '/templates/includes/list-featured-thumbnail.inc.php' ); ?>
           <!-- / FEATURED THUMBNAIL -->
 	</div>
-	<div class="content-list-block__text entry-content-inner">
+
+	
+	<div class="col-sm-8 col-md-12 article__content-col">
 		<header>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<?php get_template_part('templates/entry-meta'); ?>
@@ -18,5 +21,7 @@
 		</div>
 		<a href="<?php the_permalink(); ?>" class="btn btn--color-blue btn--size-tiny btn--arrow-right">Learn More</a>
 	</div>
+
+	
 	</article>
 </div>
